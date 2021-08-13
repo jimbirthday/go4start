055d01a86e184bc43f0ba5ed7fe96d919d526912
jim <303600370@qq.com>
2021-08-13 14:32:41 +0800

    i

--- gin-test/main2.go
+++ gin-test/main2.go
@@ -1,18 +1,16 @@
 package main
 
-import "github.com/gin-gonic/gin"
-
-func main() {
-	router := gin.Default()
-	routes := router.Group("/servicename")
-	privRoutes := routes.Group("/api/v1")
-	privRoutesMyResource := privRoutes.Group("/:paramname/myresource")
-	privRoutesMyResource.GET("/:paramname2", getResource)
-	router.Run(":8080")
-}
-
-func getResource(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"dada": "ok",
-	})
-}
+//func main() {
+//	router := gin.Default()
+//	routes := router.Group("/servicename")
+//	privRoutes := routes.Group("/api/v1")
+//	privRoutesMyResource := privRoutes.Group("/:paramname/myresource")
+//	privRoutesMyResource.GET("/:paramname2", getResource)
+//	router.Run(":8080")
+//}
+//
+//func getResource(c *gin.Context) {
+//	c.JSON(200, gin.H{
+//		"dada": "ok",
+//	})
+//}
